b14f43e827b83f1d673b8ee47397be8a359337d5
MassimoBergantin <maxbergax@gmail.com>
2021-05-31 15:19:54 +0200

    Mail/dadi

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 
 
 
-var userMail = prompt("dammi la tua email ") 
-var allMails = ['dyron@libero.it', 'adina@gmail.com', 'ottavio@gmail.com', 'bardy@hotmail.com', 'maxbergax@gmail.com']
+// var userMail = prompt("dammi la tua email ") 
+// var allMails = ['dyron@libero.it', 'adina@gmail.com', 'ottavio@gmail.com', 'bardy@hotmail.com', 'maxbergax@gmail.com']
 
 
-var check = false; 
+// var check = false; 
 
-for (var i = 0; i < allMails.length; i++) {
-    if (allMails[i] === userMail) {
-        check = true;
-    }
-}
+// for (var i = 0; i < allMails.length; i++) {
+//     if (allMails[i] === userMail) {
+//         check = true;
+//     }
+// }
 
-if (check) {
-    document.getElementById('mail').innerHTML += 'Sembra che tu sia già iscritto,Accedi ora!';
-} else {
-    document.getElementById('mail').innerHTML += 'Non ho trovato la tua mail! vuoi iscriverti?';
-}
+// if (check) {
+//     document.getElementById('mail').innerHTML += 'Sembra che tu sia già iscritto,Accedi ora!';
+// } else {
+//     document.getElementById('mail').innerHTML += 'Non ho trovato la tua mail! vuoi iscriverti?';
+// }
 
 
 
@@ -31,12 +31,12 @@ var userNum = Math.floor((Math.random() * 6) + 1);
 var pcNum = Math.floor((Math.random() * 6) + 1);
 
 if (userNum > pcNum) {
-    document.getElementById('dice').innerHTML += 'il tuo numero è ' + userNum + ', mentre il numero del computer è ' + pcNum + ' : complimenti hai vinto...per questa volta!';
+    document.getElementById('dice').innerHTML += 'complimenti hai vinto...per questa volta!';
 } else if (userNum = pcNum) {
-    document.getElementById('dice').innerHTML +=  'il tuo numero è ' + userNum + ', mentre il numero del computer è ' + pcNum + ' ... siete pari!';
+    document.getElementById('dice').innerHTML +=  '... siete pari!';
 }
 else{
-    document.getElementById('dice').innerHTML +=  'il tuo numero è ' + userNum + ', però il numero del computer è ' + pcNum + ' : La macchina ha vinto!';
+    document.getElementById('dice').innerHTML += 'La macchina ha vinto!';
 }
 
 
